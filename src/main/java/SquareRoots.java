d1bb37f5bb09c1f587ec769814ec1faa3dfb85ca
annn <avasyutina@gmail.com>
2018-10-08 14:43:36 +0200

    changes to SquareRoot class

--- src/main/java/SquareRoots.java
+++ src/main/java/SquareRoots.java
@@ -1,2 +1,21 @@
+import java.util.ArrayList;
+
 public class SquareRoots {
+    public float a;
+    public float b;
+    public float c;
+    public SquareRoots(float a, float b, float c){
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
+    public ArrayList<Float> root(float a, float b, float c){
+        float delta = b * b - (float)Math.sqrt(4 * a * c);
+        float res1 = (-b + delta) / 2 * a;
+        float res2 = (-b - delta) / 2 * a;
+        ArrayList <Float> roots = new ArrayList<>();
+        roots.add(res1);
+        roots.add(res2);
+        return roots;
+    }
 }
