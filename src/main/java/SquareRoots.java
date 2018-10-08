2801534a823f89be60402651ac94cacf96dfee58
annn <avasyutina@gmail.com>
2018-10-08 15:44:59 +0200

    main in SquareRoot class

--- src/main/java/SquareRoots.java
+++ src/main/java/SquareRoots.java
@@ -4,18 +4,25 @@ public class SquareRoots {
     public float a;
     public float b;
     public float c;
+
     public SquareRoots(float a, float b, float c){
         this.a = a;
         this.b = b;
         this.c = c;
     }
-    public ArrayList<Float> root(float a, float b, float c){
+
+    public ArrayList<Float> root(){
         float delta = (float)Math.sqrt(b * b - 4 * a * c);
-        float res1 = (-b + delta) / 2 * a;
-        float res2 = (-b - delta) / 2 * a;
+        float res1 = (-b + delta) / (2 * a);
+        float res2 = (-b - delta) / (2 * a);
         ArrayList <Float> roots = new ArrayList<>();
         roots.add(res1);
         roots.add(res2);
         return roots;
+    }
+
+    public static void main(String[] args) {
+        SquareRoots r = new SquareRoots(5, 6, 1);
+        System.out.println("The square roots of the equation are: " + r.root());
     }
 }
